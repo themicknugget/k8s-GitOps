f3f916b045cc2aad1ae9f14e1d9d87bd0455655c
Micknugget <themicknugget@gmail.com>
2023-05-26 16:33:49 -0600

    fix: improve talos provisioning

--- talos/apply.sh
+++ talos/apply.sh
@@ -12,12 +12,10 @@ do
    ENDPOINT="192.168.1.5$i"
    echo -n "$NAME - $ENDPOINT: "
    talosctl -n $ENDPOINT apply-config $INSECURE -f clusterconfig/k8s-$NAME*.yaml
+   if [[ "$i" == "1" && "$1" == "init" ]]; then
+      talosctl -n 192.168.1.51 bootstrap
+   fi
 done
-
-if [[ "$1" == "init" ]]; then
-   sleep 2
-   talosctl -n $ENDPOINT bootstrap
-fi
 
 for i in {4..6}
 do
